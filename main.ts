b6aa327e4eb987d23c5c747f65d5ca5e913f86ca
erichon <erichon18@gmail.com>
2021-02-10 22:53:25 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,25 +9,25 @@ basic.forever(function () {
     basic.pause(100)
     basic.showLeds(`
         . . . . .
+        . # # # .
         . # . # .
-        . . # . .
-        . # . # .
+        . # # # .
         . . . . .
         `)
     basic.pause(100)
     basic.showLeds(`
+        # # # # #
         # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
         # . . . #
+        # . . . #
+        # # # # #
         `)
     basic.pause(100)
     basic.showLeds(`
         . . . . .
+        . # # # .
         . # . # .
-        . . # . .
-        . # . # .
+        . # # # .
         . . . . .
         `)
     basic.pause(100)
